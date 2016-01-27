804e03d697fe45f55842fccf9c026314ea4f29ca
dfcontr <Dfdo.@Daniels-Mac-mini.local>
2016-01-27 16:36:34 -0700

    Add constants file.

---
+++ RDFeed/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  RDFeed
+//
+//  Created by Daniel Contreras on 1/27/16.
+//  Copyright © 2016 Daniel Contreras. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Web {
+        static let FeedURL = "http://hn.algolia.com/api/v1/search_by_date?query=ios"
+    }
+}
