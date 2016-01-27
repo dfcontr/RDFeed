60e1c8d0e46e597870d147a53693fb1cafe59232
dfcontr <Dfdo.@Daniels-Mac-mini.local>
2016-01-27 16:39:53 -0700

    Add AVReachability pod.

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ use_frameworks!
 
 target 'RDFeed' do
     pod 'Alamofire', '~> 3.1'
+    pod 'AVReachability'
 end
 
 target 'RDFeedTests' do
--- Pods/Target Support Files/Pods-RDFeed/Pods-RDFeed-frameworks.sh
+++ Pods/Target Support Files/Pods-RDFeed/Pods-RDFeed-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-RDFeed/AVReachability.framework"
   install_framework "Pods-RDFeed/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-RDFeed/AVReachability.framework"
   install_framework "Pods-RDFeed/Alamofire.framework"
 fi
